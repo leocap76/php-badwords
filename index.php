441f4bef15d542a8165ba47a50de886df2a183b0
Leonardo Capogna <leonardocapogna@gmail.com>
2021-01-19 17:32:16 +0100

    prova

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
   $nome = $_GET["nome"];
   $cognome = $_GET["cognome"];
 
+  $nome = "leonardo";
+  $cognome = "capogna";
+
+  echo str_replace($cognome, "***", $text);
+
   $text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
   $arrayText = explode(",", $text);
   # altro tipo di commento
@@ -30,7 +35,7 @@
     <p>Variabile GET: <?php echo $_GET["nome"]; ?></p>
     <p><?php var_dump($arrayText); ?></p>
     <h4><?php echo trim($arrayText[1]); ?></h4>
-    <?php echo str_replace("dolor", "ciao", $text); ?>
+    <?php echo str_replace("dolor", "***", $text); ?>
     <p>Lunghezza stringa: <?php echo strlen($text); ?></p>
     <h5><?php echo ucwords($text); ?></h5>
 
